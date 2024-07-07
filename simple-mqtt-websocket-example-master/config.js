dadc889f8d4c593547284b2f59ed3d6773cc1c13
TVThanh2002 <88955000+TVThanh2002@users.noreply.github.com>
2024-07-07 22:38:58 +0700

    Add files via upload

---
+++ simple-mqtt-websocket-example-master/config.js
@@ -0,0 +1,20 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = '192.168.43.13';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 8083;
+topic1 = 'Home/BedRoom/DHT22/Temperature';		// topic to subscribe to
+topic2 = 'Home/BedRoom/DHT22/Temperature';
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
